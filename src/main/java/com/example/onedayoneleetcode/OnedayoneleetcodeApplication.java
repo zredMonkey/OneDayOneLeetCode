0cad8589c24a4ed86edef41dcb92ccb44e822251
zhouhongzhan <zhz666521@163.com>
2020-07-26 14:18:11 +0800

    LeetCode Test Repository Create

---
+++ src/main/java/com/example/onedayoneleetcode/OnedayoneleetcodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.onedayoneleetcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnedayoneleetcodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnedayoneleetcodeApplication.class, args);
+    }
+
+}
