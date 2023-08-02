4bc642b8788ff04ccb9ea08752b86e4baf21af1c
zhz <=>
2023-08-03 00:22:17 +0800

    翻转单链表

---
+++ src/main/java/com/example/onedayoneleetcode/ReverseList.java
@@ -0,0 +1,31 @@
+package com.example.onedayoneleetcode;
+
+import domain.ListNode;
+
+/**
+ * 题目：
+ * 给定单链表的头节点 head ，请反转链表，并返回反转后的链表的头节点。
+ *
+ * 输入：head = [1,2,3,4,5]
+ * 输出：[5,4,3,2,1]
+ *
+ * 输入：head = [1,2]
+ * 输出：[2,1]
+ *
+ * 输入：head = []
+ * 输出：[]
+ */
+public class ReverseList {
+
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
---
+++ src/main/java/domain/ListNode.java
@@ -0,0 +1,9 @@
+package domain;
+
+import lombok.Data;
+
+@Data
+public class ListNode {
+    public ListNode next;
+    String data;
+}
