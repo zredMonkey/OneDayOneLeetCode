7723dff52e53c66b5c3073c90766dd1eddddf1a1
zhouhongzhanzhz@163.com <zhouhongzhanzhz@163.com>
2023-08-24 00:03:51 +0800

    每天一道leetcode

---
+++ src/main/java/com/example/onedayoneleetcode/mergeTwoLists.java
@@ -0,0 +1,47 @@
+package com.example.onedayoneleetcode;
+
+/**
+ * 输入两个递增排序的链表，合并这两个链表并使新链表中的节点仍然是递增排序的。
+ *
+ * 示例1：
+ *
+ * 输入：1->2->4, 1->3->4
+ * 输出：1->1->2->3->4->4
+ * 限制：
+ *
+ * 0 <= 链表长度 <= 1000
+ */
+
+
+
+public class mergeTwoLists {
+
+    public class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+    }
+
+    // 迭代法
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode preHead = new ListNode(-1);
+
+        ListNode prev = preHead;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                prev.next = l1;
+                l1 = l1.next;
+            } else {
+                prev.next = l2;
+                l2 = l2.next;
+            }
+            prev = prev.next;
+        }
+
+        // 合并后 l1 和 l2 最多只有一个还未被合并完，我们直接将链表末尾指向未合并完的链表即可
+        prev.next = l1 == null ? l2 : l1;
+
+        return preHead.next;
+    }
+
+}
